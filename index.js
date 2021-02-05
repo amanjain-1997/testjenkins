02d38dd85f20782aeda53f57bd622cfb18cd3601
amanjain-1997 <aman@ubuntu-14.04>
2021-02-05 17:01:30 +0530

    test14

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000
 app.get('/', (req, res) => {
   var new1 = isRegex(1)
   console.log(new1)
-  res.send("Jenkins Test 13")
+  res.send("Jenkins Test 14")
 })
 
 app.listen(port, () => {
